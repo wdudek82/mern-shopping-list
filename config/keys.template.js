090944d144eceac90259285565870d7456b7861b
Wojtek Dudek <wdudek82@gmail.com>
2018-06-27 22:26:25 +0200

    Initial

---
+++ config/keys.template.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://<db_user>:<db_password>@ds229909.mlab.com:29909/mern_shopping_list'
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Body-parser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
