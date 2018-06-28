1274524f6bbdfcf888eb1bad05d03cce1e779dd0
Wojtek Dudek <wdudek82@gmail.com>
2018-06-28 02:02:10 +0200

    Added reactstrap/bootstrap components to react app

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,50 @@
+// @flow
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+type State = {
+  isOpen: boolean,
+}
+
+class AppNavbar extends React.Component<null, State> {
+  state = {
+    isOpen: false
+  }
+
+  toggle = () => {
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+  }
+
+  render() {
+    return (
+      <h1>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/wdudek82">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </h1>
+    )
+  }
+}
+
+export default AppNavbar;
--- server.js
+++ server.js
@@ -21,7 +21,6 @@ mongoose.connect(db)
 app.use('/api/items', items);
 
 // Server
-// const port = process.env.PORT || 6000;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
